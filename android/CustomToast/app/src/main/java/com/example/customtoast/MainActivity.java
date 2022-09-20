f84af820c866b8bcc318ebcc3acfd7ef4a2cdd3c
unknown <Administrator@MCALABII-09.MCALABIISERVER.ORG>
2022-09-20 10:31:21 +0530

    custom toast1

---
+++ android/CustomToast/app/src/main/java/com/example/customtoast/MainActivity.java
@@ -0,0 +1,34 @@
+package com.example.customtoast;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.Gravity;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onClick(View view) {
+                //Creating the LayoutInflater instance
+                LayoutInflater li = getLayoutInflater();
+                //Getting the View object as defined in the customtoast.xml file
+                View layout = li.inflate(R.layout.customizedlayout, (ViewGroup) findViewById(R.id.custom_toast_layout));
+
+                //Creating the Toast object
+                Toast toast = new Toast(getApplicationContext());
+                toast.setDuration(Toast.LENGTH_LONG);
+                toast.setGravity(Gravity.CENTER_VERTICAL, 0, 0);
+                toast.setView(layout);//setting the view of custom toast layout
+                toast.show();
+
+    }
+}
